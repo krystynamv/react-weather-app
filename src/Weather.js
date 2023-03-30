7cd15eb2f9ecb5963c5e17e73083b02a32d9ee8a
krystynamv <moiseeva.krystyna@gmail.com>
2023-03-30 17:12:00 +0100

    Added basic styling

---
+++ src/Weather.js
@@ -0,0 +1,50 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-info w-100"
+            />
+          </div>
+        </div>
+      </form>
+      <h1>Lisbon</h1>
+      <ul>
+        <li>Wednesday 07:00</li>
+        <li>Mostly Cloudy</li>
+      </ul>
+      <div className="row mt-3">
+        <div className="col-6">
+          <img
+            src="https://www.creativefabrica.com/wp-content/uploads/2021/03/31/weather-icon-illustration03-Graphics-10205167-1-1-580x375.jpg"
+            alt="Mostly Cloudy"
+          />{" "}
+          <span className="temperature">6</span>
+          <span className="unit">°C</span>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 12%</li>
+            <li>Humidity: 72%</li>
+            <li>Wind: 10 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
