b94d511ec0108cddff23d825a61f51df247a3cef
krystynamv <moiseeva.krystyna@gmail.com>
2023-03-30 11:50:29 +0100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by Krystyna Moiseieva and is{" "}
-        <a
-          href="https://github.com/krystynamv/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by Krystyna Moiseieva and is{" "}
+          <a
+            href="https://github.com/krystynamv/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
