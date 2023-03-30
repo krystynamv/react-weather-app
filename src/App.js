5644215758330d29a205a5c89ac95fdf2371ee8f
krystynamv <moiseeva.krystyna@gmail.com>
2023-03-30 11:26:14 +0100

    .

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
       <footer>
         This project was coded by Krystyna Moiseieva and is{" "}
         <a
